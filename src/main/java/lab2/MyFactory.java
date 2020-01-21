e5e0fc64f9bef5c460b2b9b753998464c97f9b44
Guillaume <guillaume.gadeyne8@etu.univ-lorraine.fr>
2020-01-22 02:42:41 +0300

    initial

--- src/main/java/lab2/MyFactory.java
+++ src/main/java/lab2/MyFactory.java
@@ -15,9 +15,6 @@ public class MyFactory extends AbstractCandidateFactory<double[]> {
     public double[] generateRandomCandidate(Random random) {
         double[] solution = new double[dimension];
         // x from -5.0 to 5.0
-        for (int i = 0 ; i < dimension ; i++){
-            solution[i] = (random.nextDouble()-0.5)*10;
-        }
         // your implementation:
 
         return solution;
